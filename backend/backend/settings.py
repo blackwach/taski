4aad5f8541b7102b18a284dc6960b4c01cf6a09b
Denis <backwach1@yandex.ru>
2025-12-18 15:49:52 +0700

    Add Actions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,8 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','89.169.183.122', 'blackwachlearn.hopto.org']
+ALLOWED_HOSTS = ['127.0.0.1','89.169.183.122',
+'blackwachlearn.hopto.org']
 
 
 # Application definition
@@ -77,16 +78,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+	'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+	'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+	'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+	'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
